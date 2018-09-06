8e32d9a9c6b61b1b7888f0ac84a29bffe982190f
vinit <vinit@barten.biz>
2018-09-06 22:21:59 +0530

    setters for behavior class instances instead of constructor

--- src/com/vinit/Main.java
+++ src/com/vinit/Main.java
@@ -1,14 +1,20 @@
 package com.vinit;
 
 import com.vinit.statergyPattern.MallardDuck;
+import com.vinit.statergyPattern.fly.NoFly;
+import com.vinit.statergyPattern.quack.MutedQuack;
 
 public class Main {
 
     public static void main(String[] args) {
         MallardDuck mallardDuck = new MallardDuck();
+
+        mallardDuck.setFlyBehavior(new NoFly());
+        mallardDuck.setQuackBehavior(new MutedQuack());
+
         mallardDuck.display();
-        mallardDuck.performFly();
         mallardDuck.performQuack();
+        mallardDuck.performFly();
     }
 }
 
--- src/com/vinit/statergyPattern/MallardDuck.java
+++ src/com/vinit/statergyPattern/MallardDuck.java
@@ -1,6 +1,8 @@
 package com.vinit.statergyPattern;
 
 
+import com.vinit.statergyPattern.behavior.FlyBehavior;
+import com.vinit.statergyPattern.behavior.QuackBehavior;
 import com.vinit.statergyPattern.fly.FlyWithWings;
 import com.vinit.statergyPattern.quack.Squeak;
 
@@ -8,6 +10,14 @@ public class MallardDuck extends Duck {
     public MallardDuck() {
         this.flyBehavior = new FlyWithWings();
         this.quackBehavior = new Squeak();
+    }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior){
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior){
+        this.quackBehavior = quackBehavior;
     }
 
     public void display() {
